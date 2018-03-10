609b9a7cd1bd46ed2354f58e5836cfc08a5ed78b
NightWatcher <NightWatcher@DESKTOP-2KFGNNV>
2018-03-11 00:39:27 +0800

    test

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+    private Rigidbody rd;
+    public int force = 5;
+    private int score = 0;
+    public Text text;
+    public GameObject WinText;
+
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rd.AddForce(new Vector3(h,0,v)*force);
+	}
+    void OnCollisionEnter(Collision collision){
+        //string name = collision.collider.name;
+        //print(name);
+        if (collision.collider.tag == "PickUp"){
+            Destroy(collision.collider.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider collider){
+        if (collider.tag == "PickUp"){
+            score++;
+            text.text = score.ToString();
+            if(score == 8){
+                WinText.SetActive(true);
+            }
+            Destroy(collider.gameObject);
+        }
+    }
+}
